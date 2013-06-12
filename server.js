8c01832e347a717cbd194d25a4cd82960435b5a3
jimmeh <james.mcdonald@laterooms.com>
2013-06-12 11:06:41 +0100

    server now uses restify

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
-var http = require('http');
-http.createServer(function(req, res) {
-	res.writeHead(200, { 'Content-Type': 'text/plain'});
-	res.end('Hello World\n');
-}).listen(8081, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:8081');
+var restify = require("restify");
+
+var server = restify.createServer({
+	name: "Authentication"
+});
+
+server.listen(8880, function() {
+	console.log('%s listening at %s', server.name, server.url);
+});
