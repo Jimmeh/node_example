837038c245ec8912d8e0beab80f4a990fd0aea18
jimmeh <james.mcdonald@laterooms.com>
2013-06-13 16:56:37 +0100

    cleaned up directory structure.

--- app/bootstrapper.js
+++ app/bootstrapper.js
@@ -1,4 +1,3 @@
 module.exports = function(server, done) {
-	var dependencies = 1;
-	require('./authentication/route_registrar')(server, function () { if (--dependencies === 0) done();});
+	require('./authentication/basic')(server, done);
 };
--- app/router.js
+++
@@ -1,3 +0,0 @@
-module.exports = function(server, done) {
-	require('./authentication/basic')(server, done);
-};
